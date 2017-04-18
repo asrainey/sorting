8b845699b35c9a52860101ae34fbbd5accf0e5f5
Allison Rainey <allisonsrainey@gmail.com>
2017-04-18 07:20:28 -0400

    Working Program

--- sorting.rb
+++ sorting.rb
@@ -1,7 +1,27 @@
+#Had to look at solution to complete this
+def recursive_sort unsorted, sorted
+  if unsorted.length <= 0
+    return sorted
+  end
+
+  smallest = unsorted.pop
+  still_unsorted = []
+
+  unsorted.each do |object|
+    if object < smallest
+      still_unsorted.push smallest
+      smallest = object
+    else
+      still_unsorted.push object
+    end
+  end
+  sorted.push smallest
+
+  recursive_sort still_unsorted, sorted
+end
+
 def sort some_array
   recursive_sort some_array, []
 end
 
-def recursive_sort unsorted array, sorted_array
-
-end
+puts sort(['apple', 'strawberry', 'melon', 'watermelon', 'cherry', 'orange'])
