44cd0d22f0ad856fc139249a96937af021af4766
Allison Rainey <allisonsrainey@gmail.com>
2017-04-22 13:29:28 -0400

    Shuffled but losing one item

--- shuffle.rb
+++ shuffle.rb
@@ -19,24 +19,4 @@ def recursive_shuffle array
   end
 end
 
-puts recursive_shuffle(['apple', 'strawberry', 'melon', 'watermelon', 'cherry', 'orange'])
-
-#below here is prior work (for reference)
-#  while array.length > 0
-#    item = rand(array.length)
-#    current_item = 0
-#    unshuffled_array = []
-
-#    array.each do |object|
-#      if current_item == item
-#        shuffle.push object
-#      else
-#        unshuffled_array.push object
-#      end
-
-#      current_item = current_item + 1
-#    end
-#  array = unshuffled_array
-#  end
-#  shuffled
-# end
+puts recursive_shuffle ['apple', 'strawberry', 'melon', 'watermelon', 'cherry', 'orange']
